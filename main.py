fce1c6b2156c660228a1fa11253ffabef7f70eec
erik-lance <eriklance@gmail.com>
2023-12-03 01:33:08 +0800

    Focused mutex on room
    Co-Authored-By: aaronpal <ubergonmx@users.noreply.github.com>

--- main.py
+++ main.py
@@ -1,190 +1,89 @@
 import threading
-import random
 import time
+import random
 
-# Constraints
 
-# 1. There are only n slots inside the fitting room of a department store. 
-#    Thus, there can only be at most n persons inside the fitting room at a time.
+class FittingRoom:
+    def __init__(self, n):
+        self.n = n
+        self.room_sem = threading.BoundedSemaphore(n)
+        self.current_color = None
+        self.mutex = threading.Lock()
+        self.safety_mutex = threading.Lock()
+        self.condition = threading.Condition(self.mutex)
 
-# 2. There cannot be a mix of blue and green in the fitting room at the same time.
-#    Thus, there can only be at most n blue threads or at most n green threads inside
-#    the fitting room at a time.
+    def enter_room(self, thread_id, color):
+        # Acquire the lock
+        self.condition.acquire()
 
-# 3. The solution should not result in deadlock.
+        # Wait for the room to be empty or for the current color to be the same as the thread's color
+        while self.current_color is not None and self.current_color != color:
+            self.condition.wait()
 
-# 4. The solution should not result in starvation. For example, blue threads cannot 
-#    forever be blocked from entering the fitting room if green threads are lining up to enter as well.
+        # Release the lock
+        self.condition.release()
 
-# Task: Coordinate between blue and green threads.
-
-VERBOSE = True
-
-n_prompt = ""
-b_prompt = ""
-g_prompt = ""
-
-if VERBOSE:
-    print("Process Synchronization Project for CSOPESY")
-    print("Input the following: ")
-    print("     n - the number of slots inside the fitting room")
-    print("     b - number of blue threads")
-    print("     g - number of green threads")
-    print("----------------------------------------------")
-
-    n_prompt = "n: "
-    b_prompt = "b: "
-    g_prompt = "g: "
-
-n_slots = input(n_prompt)
-b_threads = input(b_prompt)
-g_threads = input(g_prompt)
-
-n_slots = int(n_slots)
-b_threads = int(b_threads)
-g_threads = int(g_threads)
-
-rng = random
-rng.seed(11)
-
-n_ctr = 0
-
-# Mutex Lock
-mutex = threading.Lock()
-bg_mutex = threading.Lock()
-
-# Semaphores
-b_sem = threading.Semaphore(n_slots)
-g_sem = threading.Semaphore(n_slots)
-
-waiting_sem = threading.Semaphore(1)
-
-is_blue_first = False
-
-threads: list[threading.Thread] = []
-
-def enter_fitting_room(thread_id, color):
-    """This enters a thread into the fitting room.
-
-    Args:
-        thread_id (int): id of the thread
-        color (string): type of thread (blue or green)
-    """
-    string_prompt = f"Thread {thread_id} ({color}) enters the fitting room."
-    
-    global n_ctr
-    global g_threads
-    global b_threads
-
-    failsafe = 5
-    i = 0
-    
-    # Start of critical section
-    mutex.acquire()
-
-    try:
-        # If the opposite color is the first to enter
-        if not is_empty():
-            if color == "blue":
-                mutex.release()
-                # Wait until green is empty
-                while(g_sem._value != n_slots):
-                    print("g_sem._value ", g_sem._value, " != ", n_slots)
-                    i+=1
-                    if i == failsafe: break
-                    pass
-                pass
-                print(string_prompt)
-                b_sem.acquire()
-            else:
-                mutex.release()
-                # Wait until blue is empty
-                while(b_sem._value != n_slots):
-                    print("b_sem._value ", b_sem._value, " != ", n_slots)
-                    i+=1
-                    if i == failsafe: break
-                    pass
-                pass
-                print(string_prompt)
-                g_sem.acquire()
-        else:
+        if self.room_sem._value == n:
+            self.room_sem.acquire()
             print(f"{color.capitalize()} only.")
-            print(string_prompt)
-            # n_ctr += 1
+        else:
+            self.room_sem.acquire()
 
-            if color == "blue":
-                b_sem.acquire()
+        with self.mutex:
+            if self.current_color is None or self.current_color == color:
+                self.current_color = color
+                print(
+                    f"Thread {thread_id} ({self.current_color}) enters the fitting room."
+                )
+                if self.is_empty():
+                    print(f"{self.current_color.capitalize()} only.")
             else:
-                g_sem.acquire()
-    except Exception as e:
-        print("Error: ", thread_id)
-    mutex.release()
+                print(
+                    f"Thread {thread_id} ({color}) waits as the fitting room is occupied by {self.current_color}."
+                )
 
-def is_empty():
-    return b_sem._value == n_slots and g_sem._value == n_slots
+    def exit_room(self, thread_id, color):
+        with self.mutex:
+            print(f"Thread {thread_id} ({color}) exits the fitting room.")
+            self.room_sem.release()
+            if self.is_empty():
+                print("Empty fitting room.")
+                self.current_color = None
+                self.condition.notify(1)
 
-def exit_fitting_room(thread_id, color):
-    """This exits a thread from the fitting room.
+    def is_empty(self):
+        return self.room_sem._value == self.n
 
-    Args:
-        thread_id (int): id of the thread
-        color (string): type of thread (blue or green)
-    """
-    string_prompt = f"Thread {thread_id} ({color}) exits the fitting room."
-    print(string_prompt)
 
-    if color == "blue":
-        b_sem.release()
-    else:
-        g_sem.release()
-    pass
+def simulate_fitting_room(n, b, g):
+    fitting_room = FittingRoom(n)
+    threads = []
 
-def create_thread(color, sleep=0):
-    """This constructs a thread that will enter the fitting room.
-
-    Args:
-        color (string): type of thread (blue or green)
-        sleep (int, optional): Changes the time the thread sleeps. Defaults to 0.
-    
-    Returns:
-        thread: the thread that will enter the fitting room
-    """
-    new_thread = threading.Thread(target=run_thread, args=(len(threads), color))
-    return new_thread
-    
-def run_thread(thread_id, color):
-    """This runs the thread.
-
-    Args:
-        thread_id (int): id of the thread
-        color (string): type of thread (blue or green)
-    """
-    print("Thread: ", thread_id, " - ", color)
-    enter_fitting_room(thread_id, color)
-    time.sleep(random.uniform(0,3))
-    exit_fitting_room(thread_id, color)
-
-def simulate_fitting_room(n, b, g, random=None):
-    """This simulates the fitting room.
-
-    Args:
-        n (int): number of slots inside the fitting room
-        b (int): number of blue threads
-        g (int): number of green threads
-        random (int): seed for randomizing how the threads enter
-    """
+    def run_thread(thread_id, color):
+        fitting_room.enter_room(thread_id, color)
+        # time.sleep(1)
+        fitting_room.exit_room(thread_id, color)
 
     for i in range(b):
-        threads.append(create_thread("blue"))
+        thread = threading.Thread(target=run_thread, args=(i, "blue"))
+        threads.append(thread)
 
     for i in range(g):
-        threads.append(create_thread("green"))
+        thread = threading.Thread(target=run_thread, args=(i + b, "green"))
+        threads.append(thread)
 
-    rng.shuffle(threads)  
-    
-    for t in threads: t.start()
-    for t in threads: t.join()
+    random.shuffle(threads)
+
+    for thread in threads:
+        thread.start()
+
+    for thread in threads:
+        thread.join()
 
 
 if __name__ == "__main__":
-    simulate_fitting_room(n_slots, b_threads, g_threads, random=11)
+    n = int(input("Enter the number of slots inside the fitting room: "))
+    b = int(input("Enter the number of blue threads: "))
+    g = int(input("Enter the number of green threads: "))
+
+    simulate_fitting_room(n, b, g)
